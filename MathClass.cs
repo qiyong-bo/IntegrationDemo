392a5f850af53873646145cb2652f4f4c8bff8cf
timqbo <timqbo@seismic.com>
2019-04-02 16:58:26 +0800

    initial commmit

---
+++ MathClass.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Tim.IntegrationDemo
+{
+    public class MathClass
+    {
+        public MathClass()
+        {
+
+        }
+
+        public double Calculate(double num1, double num2, string op)
+        {
+            double result = 0;
+            switch(op)
+            {
+               case "Add":
+                    result = num1 + num2;
+                    break;
+                case "Sub":
+                    result = num1 - num2;
+                    break;
+                case "Mul":
+                    result = num1 * num2;
+                    break;
+                case "Div":
+                    result = (double)num1 / num2;
+                    break;
+                default:
+                    break;
+            }
+            return result;
+        }
+    }
+}
